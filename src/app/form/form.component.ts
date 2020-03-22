4e2910573cd7430b1936747a8fa421cffaa4ec51
genesioamorim <amorim.genesio@gmail.com>
2020-03-22 03:42:23 -0300

    concerto do bug da tela form e implementação da estrutura

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-form',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  result: any;
+  urlToJson = {
+    "pergunta":"Qual sua idade?",
+    "tipo":"unico",
+    "calc":true,
+    "respostas":[
+        {"option" : "até 9"  , "value": 0		},
+        {"option" : "10-19"  , "value": 0.002	},
+        {"option" : "20-29"  , "value": 0.002	},
+        {"option" : "30-39"  , "value": 0.002	},
+        {"option" : "40-49"  , "value": 0.004	},
+        {"option" : "50-59"  , "value": 0.013	},
+        {"option" : "60-69"  , "value": 0.036	},
+        {"option" : "70-79"  , "value": 0.08	},
+        {"option" : "80+"    , "value": 0.148	}
+    ]
+  };
+  
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit() {
+
+    console.log(this.urlToJson); 
   }
 
 }
